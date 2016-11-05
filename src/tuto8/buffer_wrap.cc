6eb277d4b08f9596a2a0b5f3186a92f8f2ffe0c2
Ayoub serti <ayb.serti@gmail.com>
2016-11-05 00:48:06 +0000

    implement Buffer JS object; toString(), length() and setByte

--- src/tuto8/buffer_wrap.cc
+++ src/tuto8/buffer_wrap.cc
@@ -10,10 +10,9 @@
 #include "v8.h"
 #include "buffer_wrap.h"
 #include "buffer.h"
-
+#include <string>
 using namespace v8;
-//Buffer object is a function invoked by constructor `new`
-// var buffer = new Buffer(len)
+
 
 void Buffer_Wrap::RegisterBufferClass(v8::Local<v8::Object> context)
 {
@@ -21,22 +20,23 @@ void Buffer_Wrap::RegisterBufferClass(v8::Local<v8::Object> context)
 	Isolate* isolate = context->GetIsolate();
 
 	v8::Local<v8::FunctionTemplate> buffer=FunctionTemplate::New(context->GetIsolate(), New);
-	buffer->PrototypeTemplate()->SetInternalFieldCount(1);
+	buffer->InstanceTemplate()->SetInternalFieldCount(1);
 
-	buffer->PrototypeTemplate()->Set(isolate, "getLength", FunctionTemplate::New(isolate, GetLength));
-	buffer->PrototypeTemplate()->Set(isolate, "getData", FunctionTemplate::New(isolate, GetData));
+	buffer->PrototypeTemplate()->Set(isolate, "length", FunctionTemplate::New(isolate, GetLength));
+	buffer->PrototypeTemplate()->Set(isolate, "toString", FunctionTemplate::New(isolate, GetData));
 	buffer->PrototypeTemplate()->Set(isolate, "setByte", FunctionTemplate::New(isolate, SetByte));
 	
 	buffer_.Reset(isolate,buffer);
 	
 	
 	/*add buffer to context*/
-	context->Set(String::NewFromUtf8(isolate, "buffer", NewStringType::kNormal).ToLocalChecked(), buffer_.Get(isolate)->GetFunction());
+	context->Set(String::NewFromUtf8(isolate, "Buffer", NewStringType::kNormal).ToLocalChecked(), buffer_.Get(isolate)->GetFunction());
 }
 
 v8::Persistent<v8::FunctionTemplate> Buffer_Wrap::buffer_;
 
-
+//Buffer object is a function invoked by constructor `new`
+// var buffer = new Buffer(len)
 void Buffer_Wrap::New(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
 	Isolate* isolate = args.GetIsolate();
@@ -56,6 +56,7 @@ void Buffer_Wrap::New(const v8::FunctionCallbackInfo<v8::Value>& args)
 			ptr->buffer_ptr_ = new Buffer(len);
 		}
 		
+		args.GetReturnValue().Set(args.This());
 
 	}
 }
@@ -63,16 +64,76 @@ void Buffer_Wrap::New(const v8::FunctionCallbackInfo<v8::Value>& args)
 
 void Buffer_Wrap::SetByte(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
+	if (args.Length() > 1)
+	{
+		if ((args[1]->IsString() || args[1]->IsNumber())  & args[0]->IsNumber())
+		{
+			Buffer_Wrap* obj = reinterpret_cast<Buffer_Wrap*>(args.This()->GetAlignedPointerFromInternalField(0));
+			if (obj != nullptr)
+			{
+				int32_t position = args[0]->ToInteger()->Value();
+				unsigned char value = 0;
+				auto ptr = obj->buffer_ptr_;
 
+				if (args[1]->IsString())
+				{
+					String::Utf8Value utf8value(args[1]->ToString());
+					value = **utf8value;
+				}
+				else
+				{
+					value = args[1]->ToInteger()->Value() & 0xFF;
+				}
+
+				ptr->SetByte(position, value);
+				args.GetReturnValue().Set(value);
+			}
+		}
+		
+	}
+	
 }
 
 
 void Buffer_Wrap::GetLength(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
 	Buffer_Wrap* ptr = reinterpret_cast<Buffer_Wrap*>(args.This()->GetAlignedPointerFromInternalField(0));
+	args.GetReturnValue().Set((int32_t)ptr->buffer_ptr_->GetLength());
 }
 
 void Buffer_Wrap::GetData(const v8::FunctionCallbackInfo<v8::Value>& args)
 {
+	Isolate* isolate = args.GetIsolate();
+	if (args.Length() > 0)
+	{
+		if (args[0]->IsString())
+		{
+			Buffer_Wrap* obj = reinterpret_cast<Buffer_Wrap*>(args.This()->GetAlignedPointerFromInternalField(0));
+			if (obj != nullptr)
+			{
+				int32_t charset = args[0]->ToInt32()->Value();
+				auto ptr = obj->buffer_ptr_;
+				const unsigned char* data = ptr->GetData();
+				String::Utf8Value value(args[0]->ToString());
+				std::string strVal(*value);
+
+				if (strVal=="hex")
+				{
+					
+				}
+				else if (strVal == "ascii")
+				{
+					//TODO
+				}
+				else if (strVal == "utf8")
+				{
+					Local<String> ret = String::NewFromUtf8(isolate, (const char*)data, NewStringType::kNormal, ptr->GetLength()).ToLocalChecked();
+					args.GetReturnValue().Set(ret);
+				}
+				
+			}
+		}
+
+	}
 
 }
